6273303dcd1e0a9377e19714c01a8fdcf72b4e36
Ross Lindsay <ross@rosslindsay.com>
2022-01-31 00:07:13 -0500

    Add fields to signatures table

--- database/migrations/2021_12_27_030357_create_signatures_table.php
+++ database/migrations/2021_12_27_030357_create_signatures_table.php
@@ -15,6 +15,14 @@ class CreateSignaturesTable extends Migration
     {
         Schema::create('signatures', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('event_registration_id');
+            $table->string('state');
+            $table->boolean('signed_electronically')->nullable();
+            $table->string('signed_file_url')->nullable();
+            $table->string('signed_file_hash')->nullable();
+            $table->string('signing_log_url')->nullable();
+            $table->string('signing_log_file_hash')->nullable();
+            $table->timestamp('requested_at');
             $table->timestamps();
         });
     }
