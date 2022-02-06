9658b8c1da33e79eab77b1ef47af02d8f5f82df8
Ross Lindsay <ross@rosslindsay.com>
2022-02-06 10:24:18 -0500

    Re-order links to be more useful

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,11 +1,11 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('affiliation') }}'><i class='nav-icon la la-question'></i> Affiliations</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('event') }}'><i class='nav-icon la la-question'></i> Events</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('event-registration') }}'><i class='nav-icon la la-question'></i> Event registrations</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('payment') }}'><i class='nav-icon la la-question'></i> Payments</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('payment-method') }}'><i class='nav-icon la la-question'></i> Payment methods</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('signature') }}'><i class='nav-icon la la-question'></i> Signatures</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('team') }}'><i class='nav-icon la la-question'></i> Teams</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-users'></i> People</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('team') }}'><i class='nav-icon la la-user-friends'></i> Teams</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('event-registration') }}'><i class='nav-icon la la-ticket-alt'></i> Registrations</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('payment') }}'><i class='nav-icon la la-money-bill'></i> Payments</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('signature') }}'><i class='nav-icon la la-signature'></i> Signatures</a></li>
+<hr>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('event') }}'><i class='nav-icon la la-calendar-day'></i> Events</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('payment-method') }}'><i class='nav-icon la la-money-check'></i> Payment methods</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('affiliation') }}'><i class='nav-icon la la-user-tag'></i> Affiliations</a></li>
