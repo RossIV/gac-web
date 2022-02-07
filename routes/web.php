463aed0b600a3676918ab24bf36f344cf03e4085
Ross Lindsay <ross@rosslindsay.com>
2022-02-06 22:30:26 -0500

    Add route for team registration

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/team-registration', function () {
+        return view('team-registration');
+    });
+});
 
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
