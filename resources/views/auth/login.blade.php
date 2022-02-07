831fb06d204dc8d82f5c085bdcef071b5bda50b2
Ross Lindsay <ross@rosslindsay.com>
2022-02-06 23:35:36 -0500

    Bootstrap-ified login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,69 @@
-@extends('layouts.app', ['title' => 'Login'])
+@extends('layouts.app', ['title' => 'Login', 'no_head' => true])
 @section('content')
-    <div class="h-screen bg-gray-50 flex items-center justify-center">
-        <div class="w-full max-w-lg bg-white shadow-lg rounded-md p-8 space-y-4">
-            @if(!session()->has('success'))
-                <h1 class="text-xl font-semibold">Login</h1>
-                <form action="{{ route('login') }}" method="post" class="space-y-4">
-                    @csrf
-                    <div class="space-y-1">
-                        <label for="email" class="block">Email</label>
-                        <input type="email" name="email" id="email" class="block w-full border-gray-400 rounded-md px-4 py-2" />
-                        @error('email')
-                        <p class="text-sm text-red-600">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <button class="rounded-md px-4 py-2 bg-indigo-600 text-white">Login</button>
-                </form>
-            @else
-                <p>Please click the link sent to your email to finish logging in.</p>
-            @endif
+
+<main class="form-signin">
+    <h1>{{ config('app.name') }}</h1>
+    <h2 class="h3 mb-3 fw-normal">Login</h2>
+
+    @if(!session()->has('success'))
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div class="form-floating">
+                <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" value="{{old('email')}}">
+                <label for="email">Email address</label>
+                @error('email')
+                <small style="color: red">{{ $message }}</small>
+                @enderror
+            </div>
+            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Login</button>
+        </form>
+
+        <div class="row pt-3">
+            <div class="col">
+                <small>
+                    {{ config('app.name') }} uses magic links to authenticate.
+                    Simply submit your email address above, click the link in your inbox, and you're in!<br/><br/>
+                    If you're new here, don't worry - we'll create an account for you automatically.
+                </small>
+            </div>
         </div>
-    </div>
+    @else
+        <p>Please click the link sent to your email to finish logging in.</p>
+    @endif
+</main>
 @endsection
+<style>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        display: flex;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5 !important;
+    }
+
+    .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: auto;
+    }
+
+    .form-signin .checkbox {
+        font-weight: 400;
+    }
+
+    .form-signin .form-floating:focus-within {
+        z-index: 2;
+    }
+
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+</style>
