5f156c5f5f61105fd27ee4bc79401e7d275d0006
Ross Lindsay <ross@rosslindsay.com>
2022-02-11 01:13:41 -0500

    Add function to invite users to a team via email

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Mail;
+use Mpociot\Teamwork\Teamwork;
 use Mpociot\Teamwork\TeamworkTeam;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -35,5 +37,27 @@ class Team extends TeamworkTeam
             ->logOnly(['*'])
             ->logOnlyDirty()
             ->dontSubmitEmptyLogs();
+    }
+
+    /**
+     * Invite a user to join this team
+     *
+     * @param string $email
+     * @return bool
+     * @throws \Exception
+     */
+    public function invite(string $email): bool
+    {
+        if (!Teamwork::hasPendingInvite($email, $this->id)) {
+            Teamwork::inviteToTeam($email, $this, function ($invite) {
+                Mail::send('teamwork.emails.invite',
+                    ['team' => $invite->team, 'invite' => $invite], function ($m) use ($invite) {
+                    $m->to($invite->email)->subject('Invitation to join team '.$invite->team->name);
+                });
+            });
+            return true;
+        } else {
+            return false;
+        }
     }
 }
