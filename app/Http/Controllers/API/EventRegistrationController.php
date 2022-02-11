bae1b8b60f2fd23e47654e7e6d17016ddf5cd715
Ross Lindsay <ross@rosslindsay.com>
2022-02-11 01:19:40 -0500

    Started buildout of EventRegistrationController

--- app/Http/Controllers/API/EventRegistrationController.php
+++ app/Http/Controllers/API/EventRegistrationController.php
@@ -6,39 +6,46 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\API\StoreEventRegistrationRequest;
 use App\Http\Requests\API\UpdateEventRegistrationRequest;
 use App\Models\EventRegistration;
+use App\Models\Team;
+use Illuminate\Http\JsonResponse;
 
 class EventRegistrationController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(['status' => 'error', 'error' => 'Not implemented'], 501);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\API\StoreEventRegistrationRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function store(StoreEventRegistrationRequest $request)
+    public function store(StoreEventRegistrationRequest $request): JsonResponse
     {
-        //
+        // Create Team
+        // FirstOrCreate Team Members
+        // Attach Team Members to Team
+        // Get Event from Request
+        // Create Event Registration
+        // Create $0.00 Payment
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\EventRegistration  $eventRegistration
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function show(EventRegistration $eventRegistration)
+    public function show(EventRegistration $eventRegistration): JsonResponse
     {
-        //
+        return response()->json(['status' => 'error', 'error' => 'Not implemented'], 501);
     }
 
     /**
@@ -46,21 +53,21 @@ class EventRegistrationController extends Controller
      *
      * @param  \App\Http\Requests\API\UpdateEventRegistrationRequest  $request
      * @param  \App\Models\EventRegistration  $eventRegistration
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function update(UpdateEventRegistrationRequest $request, EventRegistration $eventRegistration)
+    public function update(UpdateEventRegistrationRequest $request, EventRegistration $eventRegistration): JsonResponse
     {
-        //
+        return response()->json(['status' => 'error', 'error' => 'Not implemented'], 501);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\EventRegistration  $eventRegistration
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function destroy(EventRegistration $eventRegistration)
+    public function destroy(EventRegistration $eventRegistration): JsonResponse
     {
-        //
+        return response()->json(['status' => 'error', 'error' => 'Not implemented'], 501);
     }
 }
