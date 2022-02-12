56f236338ec59b03a394948334ddcb3084677397
Ross Lindsay <ross@rosslindsay.com>
2022-02-11 22:43:49 -0500

    Add API routes for all resources

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\API\AffiliationController;
+use App\Http\Controllers\API\EventController;
+use App\Http\Controllers\API\EventRegistrationController;
+use App\Http\Controllers\API\PaymentController;
+use App\Http\Controllers\API\PaymentMethodController;
+use App\Http\Controllers\API\SignatureController;
+use App\Http\Controllers\API\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +21,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resource('affiliations', AffiliationController::class);
+    Route::resource('events', EventController::class);
+    Route::resource('eventRegistrations', EventRegistrationController::class);
+    Route::resource('payments', PaymentController::class);
+    Route::resource('paymentMethods', PaymentMethodController::class);
+    Route::resource('signatures', SignatureController::class);
+    Route::resource('teams', TeamController::class);
 });
