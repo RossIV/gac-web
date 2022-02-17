01430edb9723f7ec5bc63d2a8ad052f580eba402
Ross Lindsay <ross@rosslindsay.com>
2022-02-17 00:02:04 -0500

    Terms are gone from Team; Require all attributes for each member; Emails don't need to be unique here since we're checking for existing accounts in the controller

--- app/Http/Requests/API/StoreTeamRequest.php
+++ app/Http/Requests/API/StoreTeamRequest.php
@@ -29,14 +29,13 @@ class StoreTeamRequest extends FormRequest
             'accept_additional_members' => 'required|boolean',
             'owner_id' => 'integer|exists:users,id|prohibits:owner_email',
             'owner_email' => 'string|exists:users,email|prohibits:owner_id',
-            'terms_agreed' => 'boolean',
             'members' => 'array',
-            'members.*.first_name' => 'string',
-            'members.*.last_name' => 'string',
-            'members.*.alt_name' => 'string',
-            'members.*.email' => 'email:rfc,dns|unique:users,email',
-            'members.*.phone' => 'string',
-            'members.*.affiliation_id' => 'integer|exists:affiliations,id'
+            'members.*.first_name' => 'required|string',
+            'members.*.last_name' => 'required|string',
+            'members.*.alt_name' => 'required|string',
+            'members.*.email' => 'required|email:rfc,dns',
+            'members.*.phone' => 'required|string',
+            'members.*.affiliation_id' => 'required|integer|exists:affiliations,id'
         ];
     }
 }
