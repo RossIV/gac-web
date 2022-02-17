8a2e49392ad5fd7ff3c5edabec6010e6b0ee77d7
Ross Lindsay <ross@rosslindsay.com>
2022-02-16 23:59:45 -0500

    Define $guarded to allow for mass assignment; Define relationship with Payments model

--- app/Models/EventRegistration.php
+++ app/Models/EventRegistration.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
@@ -11,6 +12,23 @@ class EventRegistration extends Model
 {
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory, LogsActivity;
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Defines the relationship between the EventRegistration and Payment models
+     *
+     * @return MorphMany
+     */
+    public function payments(): MorphMany
+    {
+        return $this->morphMany(Payment::class, 'payable');
+    }
 
     /**
      * Returns string representation of the model
