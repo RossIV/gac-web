0bce440e3ec0d8e7e0764ddfe599f5b4d6890aab
Ross Lindsay <ross@rosslindsay.com>
2022-02-16 23:59:26 -0500

    Add Soft Deletes; Define $guarded to allow for mass assignment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,13 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class Payment extends Model
 {
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
-    use HasFactory, LogsActivity;
+    use HasFactory, LogsActivity, SoftDeletes;
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = ['id'];
 
     /**
      * Defines the polymorphic relationship between the Payment and other models
