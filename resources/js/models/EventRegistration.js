194983415449d184d5b02c5ec66c4106c1d55c55
Ross Lindsay <ross@rosslindsay.com>
2022-02-16 23:58:41 -0500

    Add models for API requests

---
+++ resources/js/models/EventRegistration.js
@@ -0,0 +1,8 @@
+import Model from './Model'
+
+export default class EventRegistration extends Model {
+    // Set the resource route of the model
+    resource() {
+        return 'eventRegistrations'
+    }
+}
