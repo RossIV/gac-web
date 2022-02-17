dbd264e1ad242f413c04c39f5d443c0b01ba14b3
Ross Lindsay <ross@rosslindsay.com>
2022-02-16 23:58:23 -0500

    Added standard payment methods

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PaymentMethod;
 use Illuminate\Database\Seeder;
 
 class PaymentMethodSeeder extends Seeder
@@ -13,6 +14,46 @@ class PaymentMethodSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $methods = [
+            [
+                'name' => 'Other - Please contact me to discuss',
+                'fee' => 0,
+                'instructions' => 'Game Control will reach out to discuss your needs.',
+                'additional_info_required' => 0,
+                'is_active' => 1
+            ],
+            [
+                'name' => 'Day Of - Any Method',
+                'fee' => 0,
+                'instructions' => 'Pay by any payment method when you arrive on game day.',
+                'additional_info_required' => 0,
+                'is_active' => 1
+            ],
+            [
+                'name' => 'In Advance - Google Pay',
+                'fee' => 0,
+                'instructions' => 'Payment will be requested from the account provided.',
+                'additional_info_required' => 1,
+                'is_active' => 1
+            ],
+            [
+                'name' => 'In Advance - Credit/Debit Card',
+                'fee' => 2.0,
+                'instructions' => 'Secure payment via Square',
+                'additional_info_required' => 0,
+                'is_active' => 1
+            ],
+            [
+                'name' => 'In Advance - Venmo',
+                'fee' => 0,
+                'instructions' => 'Payment will be requested from the account provided.',
+                'additional_info_required' => 1,
+                'is_active' => 1
+            ],
+        ];
+
+        foreach ($methods as $method) {
+            PaymentMethod::create($method);
+        }
     }
 }
