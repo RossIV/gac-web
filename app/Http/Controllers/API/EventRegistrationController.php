a7d44eeba4473c45efd50184ff4ea7314030eb7d
Ross Lindsay <ross@rosslindsay.com>
2022-02-17 00:00:23 -0500

    Moved terms to registration out of Team; Add notes; Allow payment notes

--- app/Http/Controllers/API/EventRegistrationController.php
+++ app/Http/Controllers/API/EventRegistrationController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\API\StoreEventRegistrationRequest;
 use App\Http\Requests\API\UpdateEventRegistrationRequest;
 use App\Models\EventRegistration;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class EventRegistrationController extends Controller
 {
@@ -28,22 +30,35 @@ class EventRegistrationController extends Controller
      */
     public function store(StoreEventRegistrationRequest $request): JsonResponse
     {
+        // Handle accepting terms
+        if ($request->has('terms_agreed') && $request->input('terms_agreed') == true) {
+            $terms_agreed_at = Carbon::now();
+            $terms_agreed_by = Auth::user()->id;
+        } else {
+            $terms_agreed_at = $terms_agreed_by = null;
+        }
+
         $registration = EventRegistration::create([
             'event_id' => $request->input('event_id'),
             'team_id' => $request->input('team_id'),
             'user_id' => $request->input('user_id', $request->user()->id),
+            'terms_agreed_at' => $terms_agreed_at,
+            'terms_agreed_by' => $terms_agreed_by,
+            'internal_notes' => $request->input('internal_notes'),
+            'external_notes' => $request->input('external_notes'),
         ]);
 
         if ($request->has('payment_method_id')) {
             $payment = $registration->payments()->create([
                 'payment_method_id' => $request->input('payment_method_id'),
-                'amount' => 0.00
+                'amount' => 0.00,
+                'notes' => $request->input('payment_notes')
             ]);
         }
 
         // Refresh registration after adding payment
         $registration = EventRegistration::with('payments',)->find($registration->id);
-        return response()->json(['status' => 'success', 'result' => $registration]);
+        return response()->json($registration);
     }
 
     /**
