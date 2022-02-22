fa89e6b7100cdbcaaea368a08fb6038076a817bd
Ross Lindsay <ross@rosslindsay.com>
2022-02-21 21:29:25 -0500

    Log all auth events; Set email_verified_at on first login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\LoginToken;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -28,8 +29,18 @@ class AuthController extends Controller
         $data = $request->validate([
             'email' => ['required', 'email', 'exists:users,email'],
         ]);
-        User::whereEmail($data['email'])->first()->sendLoginLink();
+        $user = User::whereEmail($data['email'])->first();
+        $user->sendLoginLink();
         session()->flash('success', true);
+
+        activity('auth')
+            ->event('login')
+            ->performedOn($user)
+            ->withProperties([
+                'ip' => $request->ip(),
+            ])
+            ->log('Magic Link Requested');
+
         return redirect()->back();
     }
 
@@ -44,14 +55,39 @@ class AuthController extends Controller
         abort_unless($request->hasValidSignature() && $token->isValid(), 401);
         $token->consume();
         Auth::login($token->user);
+
+        if ($token->user->email_verified_at == null) {
+            $token->user->email_verified_at = Carbon::now();
+            $token->user->save();
+        }
+
+        activity('auth')
+            ->event('verifyLogin')
+            ->performedOn($token->user)
+            ->withProperties([
+                'ip' => $request->ip(),
+                'method' => 'Magic Link'
+            ])
+            ->log('Successful authentication');
+
         return redirect('/');
     }
 
     /**
+     * @param Request $request
+     *
      * @return RedirectResponse
      */
-    public function logout(): RedirectResponse
+    public function logout(Request $request): RedirectResponse
     {
+        activity('auth')
+            ->event('logout')
+            ->performedOn($request->user())
+            ->withProperties([
+                'ip' => $request->ip(),
+            ])
+            ->log('Logged out');
+
         Auth::logout();
         return redirect(route('login'));
     }
