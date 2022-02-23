42ded659dc648f796f066a9203743ff018e27eed
Ross Lindsay <ross@rosslindsay.com>
2022-02-23 01:29:37 -0500

    More friendly/customized language

--- resources/views/emails/magic-login-link.blade.php
+++ resources/views/emails/magic-login-link.blade.php
@@ -1,6 +1,11 @@
 @component('mail::message')
-    Hello, to finish logging in please click the link below
-    @component('mail::button', ['url' => $url])
-        Click to login
-    @endcomponent
+Greetings from {{ config('app.admin_name') }}!
+
+To finish logging in, please click the link below.
+@component('mail::button', ['url' => $url])
+Login Now
+@endcomponent
+
+If you did not try to sign in to your {{ config('app.name') }} account, you can safely delete this email.
+If you have any questions, feel free to get in touch with {{ config('app.admin_name') }} at {{ config('app.admin_email') }}.
 @endcomponent
