18bf0ae94dabe2186e795d8e9301509c7bdde3b1
Ross Lindsay <ross@rosslindsay.com>
2022-02-26 23:23:37 -0500

    Create SignatureResource

---
+++ app/Http/Resources/SignatureResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SignatureResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'event_registration_id' => $this->event_registration_id,
+            'signed_electronically' => $this->signed_electronically,
+            'signed_file_url' => $this->signed_file_url,
+            'signed_file_hash' => $this->signed_file_hash,
+            'signing_log_url' => $this->signing_log_url,
+            'signing_log_file_hash' => $this->signing_log_file_hash,
+            'requested_at' => $this->requested_at,
+            'viewed_at' => $this->viewed_at,
+            'signed_at' => $this->signed_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'user' => UserResource::make($this->whenLoaded('user')),
+            'event_registration' => EventRegistrationResource::make($this->whenLoaded('eventRegistration'))
+        ];
+    }
+}
