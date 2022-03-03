67392b9d1a30af91a587d8d971e6bd334f38c4f5
Ross Lindsay <ross@rosslindsay.com>
2022-03-03 00:51:56 -0600

    Send emails on event/team registration creation

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\EventRegistration;
 use App\Models\Payment;
+use App\Observers\EventRegistrationObserver;
 use App\Observers\PaymentObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -30,5 +32,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         Payment::observe(PaymentObserver::class);
+        EventRegistration::observe(EventRegistrationObserver::class);
     }
 }
