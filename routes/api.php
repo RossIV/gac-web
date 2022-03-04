c95476b51634b9b69fd79312eddc72d4aa9498a0
Ross Lindsay <ross@rosslindsay.com>
2022-03-04 09:49:40 -0600

    Fix bad route namespacing

--- routes/api.php
+++ routes/api.php
@@ -21,21 +21,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::name('auth.')->prefix('auth')->group(function() {
-    Route::post('login', [AuthController::class, 'login'])->name('login');
-    Route::post('register', [AuthController::class, 'register'])->name('register');
-});
+Route::name('api.')->group(function() {
+    Route::name('auth.')->prefix('auth')->group(function() {
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::post('register', [AuthController::class, 'register'])->name('register');
+    });
 
-Route::post('signatures/callback', [SignatureController::class, 'callback']);
+    Route::post('signatures/callback', [SignatureController::class, 'callback']);
 
-Route::group(['middleware' => ['auth:sanctum']], function() {
-    Route::resource('affiliations', AffiliationController::class);
-    Route::resource('events', EventController::class);
-    Route::resource('eventRegistrations', EventRegistrationController::class);
-    Route::resource('payments', PaymentController::class);
-    Route::resource('paymentMethods', PaymentMethodController::class);
-    Route::resource('signatures', SignatureController::class);
-    Route::resource('teams', TeamController::class);
-    Route::resource('users', UserController::class);
-    Route::get('user', [UserController::class, 'showSelf']);
+    Route::group(['middleware' => ['auth:sanctum']], function() {
+        Route::resource('affiliations', AffiliationController::class);
+        Route::resource('events', EventController::class);
+        Route::resource('eventRegistrations', EventRegistrationController::class);
+        Route::resource('payments', PaymentController::class);
+        Route::resource('paymentMethods', PaymentMethodController::class);
+        Route::resource('signatures', SignatureController::class);
+        Route::resource('teams', TeamController::class);
+        Route::resource('users', UserController::class);
+        Route::get('user', [UserController::class, 'showSelf']);
+    });
 });
