984a28c7caeab49be21ad867e254bd769d436c19
Ross Lindsay <ross@rosslindsay.com>
2022-03-07 19:20:24 -0500

    Add plain text link in the email because Safari sucks

--- resources/views/emails/magic-login-link.blade.php
+++ resources/views/emails/magic-login-link.blade.php
@@ -6,6 +6,13 @@ To finish logging in, please click the link below.
 Login Now
 @endcomponent
 
+@component('mail::panel')
+**Users of some browsers (e.g. Safari) may experience issues using the button above and get an error (or see the login screen again) upon clicking.**
+If you are getting such an error, request a new link first, then _instead of clicking the button_, copy/paste the link below into your browser to log in.
+
+{{ $url }}
+@endcomponent
+
 If you did not try to sign in to your {{ config('app.name') }} account, you can safely delete this email.
 If you have any questions, feel free to get in touch with {{ config('app.admin_name') }} at {{ config('app.admin_email') }}.
 @endcomponent
